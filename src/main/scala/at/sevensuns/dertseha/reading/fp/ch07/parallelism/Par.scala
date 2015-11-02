26cdd110b52213ba7daceed5517a2926cb7cb48e
Christian Haas <christian.haas@sevensuns.at>
2015-11-02 21:51:02 +0100

    Implemented (some) final exercises

--- src/main/scala/at/sevensuns/dertseha/reading/fp/ch07/parallelism/Par.scala
+++ src/main/scala/at/sevensuns/dertseha/reading/fp/ch07/parallelism/Par.scala
@@ -12,6 +12,8 @@ object Par {
 
   def unit[A]( a: A ): Par[A] = ( es: ExecutorService ) => UnitFuture( a )
 
+  def lazyUnit[A]( a: => A ): Par[A] = fork( unit( a ) )
+
   private case class UnitFuture[A]( get: A ) extends Future[A] {
     def isDone = true
     def get( timeout: Long, units: TimeUnit ) = get
@@ -30,6 +32,9 @@ object Par {
     es => es.submit( new Callable[A] {
       def call = a( es ).get
     } )
+
+  def delay[A]( fa: => Par[A] ): Par[A] =
+    es => fa( es )
 
   def sortParViaMap2( parList: Par[List[Int]] ): Par[List[Int]] =
     map2( parList, unit( () ) )( ( a, _ ) => a.sorted )
@@ -51,4 +56,37 @@ object Par {
   def sequence[A]( ps: List[Par[A]] ): Par[List[A]] =
     ps.foldRight( unit( Nil: List[A] ) )( ( elem, parAcc ) => map2( elem, parAcc )( ( single, acc ) => single :: acc ) )
 
+  // Exercise 7.6
+  def parFilter[A]( as: List[A] )( f: A => Boolean ): Par[List[A]] = ???
+  // Where to put the synchronous filter? otherwise we'd need a flatMap
+
+  // Exercise 7.11
+  def choiceN[A]( n: Par[Int] )( choices: List[Par[A]] ): Par[A] =
+    map2( n, sequence( choices ) )( ( index, list ) => list.drop( index ).head )
+
+  def choice[A]( cond: Par[Boolean] )( t: Par[A], f: Par[A] ): Par[A] =
+    choiceN( map( cond )( result => if ( result ) 1 else 0 ) )( t :: f :: Nil )
+
+  // Exercise 7.13
+  def chooser[A, B]( pa: Par[A] )( choices: A => Par[B] ): Par[B] = ??? // need flatMap!
+  //map2( pa, unit( choices ) )( ( index, getter ) => getter( index ) )
+
+  // Exercise 7.14
+  def flatMap[A, B]( a: Par[A] )( f: A => Par[B] ): Par[B] =
+    ( es: ExecutorService ) => {
+      val af = a( es )
+      f( af.get )( es )
+    }
+
+  def join[A]( a: Par[Par[A]] ): Par[A] =
+    ( es: ExecutorService ) => {
+      val af = a( es )
+      af.get()( es )
+    }
+
+  def flatMapViaJoin[A, B]( a: Par[A] )( f: A => Par[B] ): Par[B] =
+    join( map( a )( f ) )
+
+  def joinViaFlatMap[A]( a: Par[Par[A]] ): Par[A] =
+    flatMap( a )( identity )
 }
